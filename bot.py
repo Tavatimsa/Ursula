892e5e5666822b4e5540c40f644d6716133fb0f6
Mate Balassa <mbalassa@szintezis-net.hu>
2020-11-12 15:27:09 +0100

    Welcome added

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@
 import logging, os, discord
 from dotenv import load_dotenv
 
-
-logging.basicConfig(filename='Ursula.log', level=logging.INFO)
+# logging.basicConfig(filename='Errors.log',level=logging.ERROR, format='%(asctime)s %(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -14,10 +14,23 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    logging.info(f'{client.user} has connected to Discord!\n')
+    logging.info(f'{client.user} has connected to Discord!')
 
+    guilds = ''
     for guild in client.guilds:
-            guilds = '- ' + guild.name + ' ' + str(guild.id) + '\n'
-    logging.info(f'The bot is currently member of the following guilds:\n{guilds}')
+        guilds += f'- {guild.name} {str(guild.id)}\n'
+    if guilds == '':
+        logging.error(f'The bot is currently not member of any guilds:\n')
+    else:
+        logging.info(f'The bot is currently member of the following guilds:\n{guilds}')
+
+
+@client.event
+async def on_member_join(member):
+    logging.info(f'{member} has joined.')
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Üdv a BloodTracken, {member.name}! Remélem, jól érzed majd magad nálunk :)'
+    )
 
 client.run(TOKEN)
